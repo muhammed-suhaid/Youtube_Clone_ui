9390d33aa6030465845c52f953eaeabdd865dcd4
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-06-24 11:18:26 +0530

    created a splash screen and a bottomnavbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone_ui/pages/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Youtube Clone',
       debugShowCheckedModeBanner: false,
-      home: ,
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone_ui/pages/bottom_nav_bar.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const BottomNavBar(),
+          ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0x00121212),
+      body: Center(
+        child: ColorFiltered(
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.5),
+            BlendMode.srcATop,
+          ),
+          child: Image.asset('assets/images/logo.png'),
+        ),
+      ),
+    );
+  }
+}
