e45d43db6bc57ba3b9bbfd9deb524e378bc1ee3c
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-06-24 17:15:08 +0530

    home_screen completed, working clean

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -87,7 +87,7 @@ class HomeScreen extends StatelessWidget {
               ),
               const SizedBox(height: 10),
               ShortsCard(shortsData: shortsData),
-              const SizedBox(height: 10),
+              const SizedBox(height: 20),
               VideoCard(videoData: videoData), 
             ],
           ),
