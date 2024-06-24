96e252d6637831bb9fe3b20ba73deeaa5a7c13f8
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-06-24 10:54:14 +0530

    created a model

---
+++ lib/models/video.dart
@@ -0,0 +1,19 @@
+class Video {
+  final String image;
+  final String title;
+  final String accountName;
+  final String profilePhoto;
+  final String views;
+  final String uploadedTime;
+  final bool isShorts;
+
+  Video({
+    required this.image,
+    required this.title,
+    required this.accountName,
+    required this.profilePhoto,
+    required this.views,
+    required this.uploadedTime,
+    required this.isShorts,
+  });
+}
